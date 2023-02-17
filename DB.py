31ae6acf5a9668b19884742db2c46d4f91398333
medwuu <aepegon@gmail.com>
2023-02-17 23:09:58 +0300

    ееее

--- DB.py
+++ DB.py
@@ -9,7 +9,6 @@ def newUser(name, telegramID):
     cur.execute(f"""INSERT INTO interns VALUES
         ('{name}', '{datetime.date.today()}, '{telegramID}') """)
     con.commit()
-    cur.execute("""CREATE TABLE IF NOT EXIST """)
 
 def deleteUser(telegramID):
     cur.execute(f"""DELETE FROM interns WHERE TelegramID = '{telegramID}'""")
