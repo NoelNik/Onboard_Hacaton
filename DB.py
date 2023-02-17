5a97ce21de6733f0f963d9fb01bdc941752038b5
ezzh3 <ezzh32@yandex.ru>
2023-02-17 21:34:21 +0300

    new and delete user

--- DB.py
+++ DB.py
@@ -1,3 +1,17 @@
 import sqlite3
+import datetime
 
-sqlite3.connect("baza.db")
+con = sqlite3.connect("baza.db")
+cur = con.cursor()
+
+
+def newUser(name):
+    cur.execute(f"""INSERT INTO interns VALUES
+        ('{name}', '{datetime.date.today()}') """)
+    con.commit()
+
+
+def deleteUser(name):
+    cur.execute(f"""DELETE FROM interns WHERE name = '{name}'""")
+    con.commit()
+
