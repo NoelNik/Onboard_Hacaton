b74d63044fe6a9ac655e2c8df624760820c24ec5
NoelNik <leon0208042@gmail.com>
2023-02-18 00:33:06 +0300

    add_chatHR

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 from config import TOKEN
+from DB import if_user_exist, getExp, getTask, addTask, getData
 
 
 
@@ -8,6 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+
     bot.send_message(message.chat.id, "Привет! Я буду твоим помошником для удобной адаптации к новой рабочей среде")
     sti = open('stickers/sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, sti)
@@ -25,19 +27,29 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def message_echo(message):
     if message.text == "Рассказать про нашу компанию":
-        bot.send_message(message.chat.id, "nu tipa sosi")
+        bot.send_message(message.chat.id, "Перейди по ссылке за всей нужной информацией :)")
+        bot.send_message(message.chat.id, "https://disk.yandex.ru/d/SO7F8r0xI3DAsg")
+
     elif message.text == "Показать свой профиль":
-        bot.send_message(message.chat.id, "nekit lox")
+        bot.send_message(message.chat.id, f"{message.chat.first_name}, вот сколько у тебя баллов: {getExp(message.chat.id)}")
+
     elif message.text == "Связь с HR":
         bot.send_message(message.chat.id, "a ti ne ofigel")
+
     elif message.text == "Показать задания":
-        bot.send_message(message.chat.id, "net, otdixai")
+        bot.send_message(message.chat.id, f"Вот список твоих заданий: {getTask}")
+
     else:
         item1 = types.KeyboardButton('/start')
         item2 = types.KeyboardButton('/help')
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         markup.add(item1, item2)
         bot.send_message(message.chat.id, "Я не понял вашу команду", reply_markup=markup)
+
+@bot.message_handler(commands=['Связь с HR'])
+def chat_hr(message):
+    pass
+
 
 def start():
     bot.infinity_polling()
